fe9e052a5f3e3e37a97978c71d53e0973167c7de
Yury Tilis <YTilis@gmail.com>
2015-02-26 23:52:03 -0500

    Moved some stuff in preperation for combining the files later

--- alarm.sh
+++ alarm.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 MICROPHONE='QuickCam Pro 9000'
-HWDEVICE=$(arecord -l | grep "$MICROPHONE"  | awk '{ gsub(":",""); print $2}')
-SAMPLE_DURATION=5 # seconds
-FORMAT='CD'       # this is the format that my USB microphone generates
+SAMPLE_DURATION=2 # seconds
+FORMAT='CD'
 THRESHOLD=0.9
+
+HWDEVICE=$(arecord -l | grep "$MICROPHONE"  | awk '{ gsub(":",""); print $2}')
 
 while :
 do
   AMPLITUDE=$(arecord -D plughw:$HWDEVICE,0 -d $SAMPLE_DURATION -f $FORMAT 2>/dev/null | sox -t .wav - -n stat 2>&1 | grep 'Maximum amplitude:'  | awk '{print $3}')
 
   # Echo the Amplitude if --verbose is set
-  # echo $AMPLITUDE
 
   COMPARE=$(echo "$AMPLITUDE > $THRESHOLD" | bc)
 
   if [ $COMPARE -eq 1 ]; then
-    echo "Hit Threshold!"
-    # ./push.sh
+    echo "Audio Detected! : Level $AMPLITUDE"
+    ./push.sh
   else
-    echo "No audio"
+    echo "No audio : Level $AMPLITUDE"
   fi
 done
