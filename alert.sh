f9a7dfef92e2cbaf29ea787c0380706a3350e6f6
Yury Tilis <YTilis@gmail.com>
2015-03-04 21:58:44 -0500

    -Made it loop over a list of push targets to notify

--- alert.sh
+++ alert.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function parse_json()
+{
+    echo $1 | sed -e 's/[{}]/''/g' | awk -F=':' -v RS=',' "\$1~/\"$2\"/ {print}" | sed -e "s/\"$2\"://" | tr -d "\n\t" | sed -e 's/\\"/"/g' | sed -e 's/\\\\/\\/g' | sed -e 's/^[ \t]*//g' | sed -e 's/^"//'  -e 's/"$//'
+}
+
 source alert.cfg
 
 HWDEVICE=$(arecord -l | grep "$MICROPHONE"  | awk '{ gsub(":",""); print $2}')
@@ -14,7 +19,34 @@ do
 
   if [ $COMPARE -eq 1 ]; then
     echo "Audio Detected! : Level $AMPLITUDE"
-    curl -u $PB_API: https://api.pushbullet.com/v2/pushes -d type=note -d title="$PB_TITLE" -d body="$PB_MSG"
+
+    IFS=:
+    while read name token
+    do
+      OUT=$(curl --silent --show-error --write-out '\n%{http_code}' -u $token: https://api.pushbullet.com/v2/pushes -d type=note -d title="$PB_TITLE" -d body="$PB_MSG") 2>/dev/null
+
+     # parse_json '{"username":"john doe","email":"john@doe.com"}' email
+
+     # get exit code
+      RET=$?
+
+      if [[ $RET -ne 0 ]] ; then
+        # if error exit code, print exit code
+        echo "Error $RET"
+
+        # print HTTP error
+        echo "HTTP Error: $(echo "$OUT" | tail -n1 )"
+      else
+        # otherwise print last line of output, i.e. HTTP status code
+        echo "Success, HTTP status is:"
+        echo "$OUT" | tail -n1
+
+        # and print all but the last line, i.e. the regular response
+        echo "Response is:"
+        echo "$OUT" | head -n-1
+      fi
+
+    done < targets.txt
   else
     echo "No audio : Level $AMPLITUDE"
   fi
